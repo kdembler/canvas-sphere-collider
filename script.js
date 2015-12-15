2c323324591263c9a0bae564da3f5606398959a7
Klaudiusz Dembler <k.dembler@gmail.com>
2015-12-15 15:59:00 +0100

    Initial page layour, working canvas drawing

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.write("I'm working");
+
+function drawTest() {
+  var canvas = document.getElementById("mainCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#000000";
+  ctx.beginPath();
+  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+}
